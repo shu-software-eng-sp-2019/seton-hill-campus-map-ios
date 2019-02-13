181a4d540538753d8def4d9471213a9e69dd8a74
Coltin Kifer <ck108860@yahoo.com>
2019-02-13 12:11:48 -0500

    Initial Commit

---
+++ shu-campus-map-iOS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  shu-campus-map-iOS
+//
+//  Created by Coltin Kifer on 2/13/19.
+//  Copyright © 2019 Coltin Kifer. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
