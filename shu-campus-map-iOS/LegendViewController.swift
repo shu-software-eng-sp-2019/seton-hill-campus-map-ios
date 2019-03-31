4ec423e9c4677bc2608bf57903921bc005ae04c8
Coltin Kifer <ck108860@yahoo.com>
2019-03-31 02:07:56 -0400

    add dummy data to table view

--- shu-campus-map-iOS/LegendViewController.swift
+++ shu-campus-map-iOS/LegendViewController.swift
@@ -11,10 +11,25 @@ import Pulley
 
 class LegendViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet var gripperView: UIView!
+    
+    @IBOutlet var gripperTopConstraint: NSLayoutConstraint!
+    
+    fileprivate var drawerBottomSafeArea: CGFloat = 0.0 {
+        didSet {
+            self.loadViewIfNeeded()
+            
+            // We'll configure our UI to respect the safe area. In our small demo app, we just want to adjust the contentInset for the tableview.
+            tableView.contentInset = UIEdgeInsets(top: 0.0, left: 0.0, bottom: drawerBottomSafeArea, right: 0.0)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        gripperView.layer.cornerRadius = 2.5
     }
     
 
@@ -34,7 +49,7 @@ extension LegendViewController: PulleyDrawerViewControllerDelegate {
     func collapsedDrawerHeight(bottomSafeArea: CGFloat) -> CGFloat
     {
         // For devices with a bottom safe area, we want to make our drawer taller. Your implementation may not want to do that. In that case, disregard the bottomSafeArea value.
-        return 48.0 + (pulleyViewController?.currentDisplayMode == .drawer ? bottomSafeArea : 0.0)
+        return 10.0 + (pulleyViewController?.currentDisplayMode == .drawer ? bottomSafeArea : 0.0)
     }
     
     func partialRevealDrawerHeight(bottomSafeArea: CGFloat) -> CGFloat
@@ -56,5 +71,33 @@ extension LegendViewController: PulleyDrawerViewControllerDelegate {
     /// This function is called when the current drawer display mode changes. Make UI customizations here.
     func drawerDisplayModeDidChange(drawer: PulleyViewController) {
         // do something
+    }
+}
+
+extension LegendViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "SampleCell", for: indexPath)
+    }
+}
+
+extension LegendViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 81.0
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let primaryContent = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PrimaryTransitionTargetViewController")
+        
+        pulleyViewController?.setDrawerPosition(position: .collapsed, animated: true)
+        
+        pulleyViewController?.setPrimaryContentViewController(controller: primaryContent, animated: false)
     }
 }
