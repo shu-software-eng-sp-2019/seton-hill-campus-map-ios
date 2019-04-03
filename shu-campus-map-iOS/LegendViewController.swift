1591ea34e159eaa083050fd059c0e3abb4bb6364
Coltin Kifer <ck108860@yahoo.com>
2019-04-03 14:22:51 -0400

    fix rotation pulley menu

--- shu-campus-map-iOS/LegendViewController.swift
+++ shu-campus-map-iOS/LegendViewController.swift
@@ -13,8 +13,13 @@ class LegendViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet var gripperView: UIView!
+    @IBOutlet weak var topSeparatorView: UIView!
+    @IBOutlet weak var bottomSeparatorView: UIView!
     
     @IBOutlet var gripperTopConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var headerSectionHeightConstraint: NSLayoutConstraint!
+    
     
     fileprivate var drawerBottomSafeArea: CGFloat = 0.0 {
         didSet {
@@ -49,7 +54,7 @@ extension LegendViewController: PulleyDrawerViewControllerDelegate {
     func collapsedDrawerHeight(bottomSafeArea: CGFloat) -> CGFloat
     {
         // For devices with a bottom safe area, we want to make our drawer taller. Your implementation may not want to do that. In that case, disregard the bottomSafeArea value.
-        return 10.0 + (pulleyViewController?.currentDisplayMode == .drawer ? bottomSafeArea : 0.0)
+        return 50 + (pulleyViewController?.currentDisplayMode == .drawer ? bottomSafeArea : 0.0)
     }
     
     func partialRevealDrawerHeight(bottomSafeArea: CGFloat) -> CGFloat
@@ -65,12 +70,44 @@ extension LegendViewController: PulleyDrawerViewControllerDelegate {
     // This function is called by Pulley anytime the size, drawer position, etc. changes. It's best to customize your VC UI based on the bottomSafeArea here (if needed). Note: You might also find the `pulleySafeAreaInsets` property on Pulley useful to get Pulley's current safe area insets in a backwards compatible (with iOS < 11) way. If you need this information for use in your layout, you can also access it directly by using `drawerDistanceFromBottom` at any time.
     func drawerPositionDidChange(drawer: PulleyViewController, bottomSafeArea: CGFloat)
     {
-        // do something
+        // We want to know about the safe area to customize our UI. Our UI customization logic is in the didSet for this variable.
+        drawerBottomSafeArea = bottomSafeArea
+        
+        /*
+         Some explanation for what is happening here:
+         1. Our drawer UI needs some customization to look 'correct' on devices like the iPhone X, with a bottom safe area inset.
+         2. We only need this when it's in the 'collapsed' position, so we'll add some safe area when it's collapsed and remove it when it's not.
+         3. These changes are captured in an animation block (when necessary) by Pulley, so these changes will be animated along-side the drawer automatically.
+         */
+        if drawer.drawerPosition == .collapsed
+        {
+            headerSectionHeightConstraint.constant = 68.0 + drawerBottomSafeArea
+        }
+        else
+        {
+            headerSectionHeightConstraint.constant = 68.0
+        }
+        
+        // Handle tableview scrolling / searchbar editing
+        
+        tableView.isScrollEnabled = drawer.drawerPosition == .open || drawer.currentDisplayMode == .panel
+        
+        if drawer.currentDisplayMode == .panel
+        {
+            topSeparatorView.isHidden = drawer.drawerPosition == .collapsed
+            bottomSeparatorView.isHidden = drawer.drawerPosition == .collapsed
+        }
+        else
+        {
+            topSeparatorView.isHidden = false
+            bottomSeparatorView.isHidden = true
+        }
     }
     
     /// This function is called when the current drawer display mode changes. Make UI customizations here.
     func drawerDisplayModeDidChange(drawer: PulleyViewController) {
-        // do something
+        print("Drawer: \(drawer.currentDisplayMode)")
+        gripperTopConstraint.isActive = drawer.currentDisplayMode == .drawer
     }
 }
 
